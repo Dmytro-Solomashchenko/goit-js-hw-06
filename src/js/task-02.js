44e51af08029e4e3522f9856f45742f05a303a45
Dmytro_Solomashchenko <solomashchenko@me.com>
2022-07-30 20:26:03 +0300

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('ul');
+const listRef = [];
+
+ingredients.forEach(ingredient => {
+  const item = document.createElement('li');
+  item.classList.add('item');
+  item.textContent = ingredient;
+  listRef.push(item);
+});
+listEl.append(...listRef);
