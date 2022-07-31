cb70ad739c70715ee45241dbc68d4781d1eea26a
Dmytro_Solomashchenko <solomashchenko@me.com>
2022-07-31 14:06:36 +0300

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,13 @@
-
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === '' || password.value === '') {
+    alert('!!! ВСЕ ПОЛЯ ДОЛЖНЫ БЫТЬ ЗАПОЛНЕНЫ !!!');
+  }
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
