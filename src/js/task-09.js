854d1115ee9f989849a0f4f75162713deacab415
Dmytro_Solomashchenko <solomashchenko@me.com>
2022-07-31 18:22:33 +0300

    update 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bodyEl: document.querySelector('body'),
+  buttonEl: document.querySelector('.change-color'),
+  spanEl: document.querySelector('.color'),
+};
+
+refs.buttonEl.addEventListener('click', onChangeColor);
+
+function onChangeColor(event) {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  refs.spanEl.textContent = getRandomHexColor();
+}
